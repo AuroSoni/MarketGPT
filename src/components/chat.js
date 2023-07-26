93222a365ecdd2bf406f5789e76a0b72602c3582
Auro <aurokumar926@gmail.com>
2023-07-26 23:58:54 +0530

    ui corrections

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, IconButton, Container, Paper, Typography, Box } from '@mui/material';
+import { TextField, IconButton, Container, Paper, Typography, Box, Grid } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import CircularProgress from '@mui/material/CircularProgress';
 
@@ -35,25 +35,29 @@ const Chat = () => {
     }
 
     return (
-        <>
-            <Container maxWidth="xl" sx={{ marginTop: '5rem' }}>
-                <Box>
-                    {data.map((item, index) => (
-                        <Paper key={index} sx={{
-                            padding: '1rem',
-                            marginBottom: '1rem',
-                            alignSelf: item.type === 'Prompt' ? 'flex-end' : 'flex-start',
-                            backgroundColor: item.type === 'Prompt' ? '#DCF8C6' : '#FFF',
-                            maxWidth: '70%',
-                            marginLeft: item.type === 'Prompt' ? 'auto' : '0',
-                            marginRight: item.type === 'Prompt' ? '0' : 'auto',
-                            borderRadius: '1rem'
-                        }}>
-                            <Typography variant="subtitle1" color="textSecondary">{item.type}</Typography>
-                            <Typography variant="body1" dangerouslySetInnerHTML={{__html: item.text}} />
-                        </Paper>
-                    ))}
-                </Box>
+        <Grid container direction="column" style={{height: "100vh", alignItems:"center"}}>
+            <Grid item style={{overflow: 'auto', marginBottom: "75px", width: '80%'}}>
+                <Container maxWidth="xl" sx={{ marginTop: '5rem' }}>
+                    <Box>
+                        {data.map((item, index) => (
+                            <Paper key={index} sx={{
+                                padding: '1rem',
+                                marginBottom: '1rem',
+                                alignSelf: item.type === 'Prompt' ? 'flex-end' : 'flex-start',
+                                backgroundColor: item.type === 'Prompt' ? '#DCF8C6' : '#FFF',
+                                maxWidth: '70%',
+                                marginLeft: item.type === 'Prompt' ? 'auto' : '0',
+                                marginRight: item.type === 'Prompt' ? '0' : 'auto',
+                                borderRadius: '1rem'
+                            }}>
+                                <Typography variant="subtitle1" color="textSecondary">{item.type}</Typography>
+                                <Typography variant="body1" dangerouslySetInnerHTML={{__html: item.text}} />
+                            </Paper>
+                        ))}
+                    </Box>
+                </Container>
+            </Grid>
+            <Grid item style={{position: 'fixed', bottom: 10, width: '80%'}}>
                 <Box>
                     <form onSubmit={handleSubmit}>
                         <TextField
@@ -72,8 +76,8 @@ const Chat = () => {
                         />
                     </form>
                 </Box>
-            </Container>
-        </>
+            </Grid>
+        </Grid>
     );
 }
 
